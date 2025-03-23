dac9657672e1eca2fafea0264740eee8d193366b
iamshahzad52 <shahzadsahib82@gmail.com>
2025-03-23 13:11:31 +0500

    Add MongoDB connection

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+# MongoDB connection
+client = MongoClient("mongodb://localhost:27017/")
+db = client["url_shortener"]
+urls_collection = db["urls"]
 
 @app.route('/')
 def home():
